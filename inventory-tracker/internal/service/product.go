637966677760ba4f24f1b7cefbe5f5a069c64a10
James <james.crstpr@gmail.com>
2026-09-05 15:34:29 +0700

    feat: create product services

---
+++ inventory-tracker/internal/service/product.go
@@ -0,0 +1,34 @@
+package service
+
+import (
+	"context"
+	"github.com/Crstpr/inventory-tracker/internal/dbgen"
+	"github.com/Crstpr/inventory-tracker/internal/repository"
+	"github.com/jackc/pgx/v5/pgtype"
+)
+
+type productUseCase struct {
+	productRepository repository.ProductRepositoryInterface
+}
+
+func NewProductService(productRepository repository.ProductRepositoryInterface) ProductServiceInterface {
+	return &productUseCase{
+		productRepository: productRepository,
+	}
+}
+
+func (s *productUseCase) ListProducts(ctx context.Context) ([]dbgen.ListProductsRow, error) {
+	products, err := s.productRepository.ListProducts(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return products, nil
+}
+
+func (s *productUseCase) GetProductByID(ctx context.Context, id pgtype.UUID) (dbgen.GetProductByIDRow, error) {
+	product, err := s.productRepository.GetProductByID(ctx, id)
+	if err != nil {
+		return dbgen.GetProductByIDRow{}, err
+	}
+	return product, nil
+}
---
+++ inventory-tracker/internal/service/product_interface.go
@@ -0,0 +1,12 @@
+package service
+
+import (
+	"context"
+	"github.com/Crstpr/inventory-tracker/internal/dbgen"
+	"github.com/jackc/pgx/v5/pgtype"
+)
+
+type ProductServiceInterface interface {
+	ListProducts(ctx context.Context) ([]dbgen.ListProductsRow, error)
+	GetProductByID(ctx context.Context, id pgtype.UUID) (dbgen.GetProductByIDRow, error)
+}
