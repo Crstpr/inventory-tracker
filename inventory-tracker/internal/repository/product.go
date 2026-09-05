cefa5c7cdaa90894b0ce474ba9f9e0973cf2b1a9
James <james.crstpr@gmail.com>
2026-09-05 15:22:17 +0700

    feat: update repository

--- inventory-tracker/internal/repository/product.go
+++ inventory-tracker/internal/repository/product.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/Crstpr/inventory-tracker/internal/dbgen"
+	"github.com/jackc/pgx/v5/pgtype"
 )
 
 type productRepository struct {
@@ -19,4 +20,8 @@ func NewProductRepository(queries *dbgen.Queries) ProductRepositoryInterface {
 
 func (r *productRepository) ListProducts(ctx context.Context) ([]dbgen.ListProductsRow, error) {
 	return r.queries.ListProducts(ctx)
+}
+
+func (r *productRepository) GetProductByID(ctx context.Context, id pgtype.UUID) (dbgen.GetProductByIDRow, error) {
+	return r.queries.GetProductByID(ctx, id)
 }
--- inventory-tracker/internal/repository/product_interface.go
+++ inventory-tracker/internal/repository/product_interface.go
@@ -2,9 +2,12 @@ package repository
 
 import (
 	"context"
+
 	"github.com/Crstpr/inventory-tracker/internal/dbgen"
+	"github.com/jackc/pgx/v5/pgtype"
 )
 
 type ProductRepositoryInterface interface {
 	ListProducts(ctx context.Context) ([]dbgen.ListProductsRow, error)
+	GetProductByID(ctx context.Context, id pgtype.UUID) (dbgen.GetProductByIDRow, error)
 }
